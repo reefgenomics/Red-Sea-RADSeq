c4fde8bce8bd78ef7ed914a6d12e141e9b3a64e6
didillysquat <didillysquat@gmail.com>
2021-04-27 09:43:38 +0100

    do hierarchical plot split by species

--- ITS2/buitrago.py
+++ ITS2/buitrago.py
@@ -213,6 +213,159 @@ class BuitragoOrdinations(Buitrago):
             return
 
 
+class BuitragoHier_split_species(Buitrago):
+    """
+    Plot up a series of dendrograms
+    This dendogram will be split by species and we will perform clustering for each species and plot this up as well
+    """
+    def __init__(self, dist_type='bc'):
+        super().__init__(dist_type=dist_type)
+
+        # setup fig
+        # 6 rows for the dendro and 1 for the coloring by species
+        gs = gridspec.GridSpec(nrows=19, ncols=2)
+
+        self.fig = plt.figure(figsize=(self._mm2inch(183, 80)))
+        self.dendro_ax_spis = plt.subplot(gs[:8, :1])
+        self.seq_bars_ax_spis = plt.subplot(gs[8:12, :1])
+        self.prof_bars_ax_spis = plt.subplot(gs[12:16, :1])
+        self.region_ax_spis = plt.subplot(gs[16:18, :1])
+
+
+        self.dendro_ax_pver = plt.subplot(gs[:8, 1:2])
+        self.seq_bars_ax_pver = plt.subplot(gs[8:12, 1:2])
+        self.prof_bars_ax_pver = plt.subplot(gs[12:16, 1:2])
+        self.region_ax_pver = plt.subplot(gs[16:18, 1:2])
+
+
+        self.region_ax_legend = plt.subplot(gs[18:19, :])
+        # TODO make an overall braycurtis matrix instead of just symbiodinium and try working with this.
+        self.symbiodinium_host_names_spis = [_ for _ in self.symbiodinium_host_names if _[0] == "S"]
+        self.sph_spis = SPHierarchical(
+            dist_output_path=self.symbiodinium_dist_path, ax=self.dendro_ax_spis,
+            sample_names_included=self.symbiodinium_host_names_spis)
+        self.sph_spis.plot()
+        self.dendro_ax_spis.collections[0].set_linewidth(0.5)
+        # self.dendro_ax_spis.set_ylim(0,0.8)
+        self.dendro_ax_spis.set_title("S. pistillata", style='italic', fontsize='small')
+
+        self.symbiodinium_host_names_pver = [_ for _ in self.symbiodinium_host_names if _[0] == "P"]
+        self.sph_pver = SPHierarchical(
+            dist_output_path=self.symbiodinium_dist_path, ax=self.dendro_ax_pver,
+            sample_names_included=self.symbiodinium_host_names_pver)
+        self.sph_pver.plot()
+        self.dendro_ax_pver.collections[0].set_linewidth(0.5)
+        # self.dendro_ax_pver.set_ylim(0, 0.8)
+        self.dendro_ax_pver.set_title("P. verrucosa", style='italic', fontsize='small')
+
+        # We will hardcode the x coordinates as they seem to be standard for the dendrogram plots
+        self.x_coords_spis = range(5, (len(self.sph_spis.dendrogram['ivl']) * 10) + 5, 10)
+        self.sample_name_to_x_coord_dict_spis = {
+            sample_name: x_coord for
+            sample_name, x_coord in
+            zip(self.sph_spis.dendrogram['ivl'], self.x_coords_spis)
+        }
+
+        self.x_coords_pver = range(5, (len(self.sph_pver.dendrogram['ivl']) * 10) + 5, 10)
+        self.sample_name_to_x_coord_dict_pver = {
+            sample_name: x_coord for
+            sample_name, x_coord in
+            zip(self.sph_pver.dendrogram['ivl'], self.x_coords_pver)
+        }
+
+        self._plot_meta_info_ax(ax=self.region_ax_spis, meta='region',  name_to_coord_dict=self.sample_name_to_x_coord_dict_spis, x_coords=self.x_coords_spis)
+
+
+        self._plot_meta_info_ax(ax=self.region_ax_pver, meta='region',
+                                name_to_coord_dict=self.sample_name_to_x_coord_dict_pver, x_coords=self.x_coords_pver)
+
+        self._plot_region_leg_ax()
+
+        self.plot_bars(sphist=self.sph_spis, bar_ax=self.seq_bars_ax_spis, seq=True)
+        self.plot_bars(sphist=self.sph_pver, bar_ax=self.seq_bars_ax_pver, seq=True)
+        self.plot_bars(sphist=self.sph_spis, bar_ax=self.prof_bars_ax_spis, seq=False)
+        self.plot_bars(sphist=self.sph_pver, bar_ax=self.prof_bars_ax_pver, seq=False)
+
+        plt.savefig(f'dendro_bars_{dist_type}.species.split.svg')
+        plt.savefig(f'dendro_bars_{dist_type}.species.split.png', dpi=1200)
+
+    def _plot_region_leg_ax(self):
+        self.region_ax_legend.set_xlim(0, 1)
+        self.region_ax_legend.set_ylim(0, 1)
+        r = []
+        for i, region in enumerate(reversed(self.regions)):
+            r.append(Rectangle((i * 0.16, 0), 0.08, 1, color=self.region_color_dict[region]))
+            self.region_ax_legend.text(x=i * 0.16 + 0.09, y=0.5, s=region, va='center', fontsize='xx-small')
+        patches_collection = PatchCollection(r, match_original=True)
+        self.region_ax_legend.add_collection(patches_collection)
+        self.region_ax_legend.set_xticks([])
+        self.region_ax_legend.set_yticks([])
+        self.region_ax_legend.spines['right'].set_visible(False)
+        self.region_ax_legend.spines['left'].set_visible(False)
+        self.region_ax_legend.spines['top'].set_visible(False)
+        self.region_ax_legend.spines['bottom'].set_visible(False)
+        self.region_ax_legend.text(x=0, y=-0.5, s="southernmost", va='center', fontsize='xx-small')
+        self.region_ax_legend.text(x=0.8, y=-0.5, s="northernmost", va='center', fontsize='xx-small')
+
+    def plot_bars(self, sphist, bar_ax, seq):
+        # We want to plot the bars in the order of the hierarchical
+        # we will use the sph.dendrogram['ivl'] uids converted to names
+        dendrogram_sample_name_order = [self.symbiodinium_sample_uid_to_sample_name_dict[_] for _ in sphist.dendrogram['ivl']]
+        # Now plot the bars
+        if seq:
+            plot_type = "seq_only"
+        else:
+            plot_type = "profile_only"
+        spb = SPBars(
+            seq_count_table_path=self.seq_count_table_path,
+            profile_count_table_path=self.profile_count_table_path,
+            plot_type=plot_type, orientation='h', legend=False, relative_abundance=True,
+            sample_names_included=dendrogram_sample_name_order, bar_ax=bar_ax, limit_genera=['A']
+        )
+        spb.plot()
+        bar_ax.set_xticks([])
+        bar_ax.set_yticks([])
+        if seq:
+            bar_ax.set_ylabel('sequences', rotation='vertical', fontsize='xx-small')
+        else:
+            bar_ax.set_ylabel('profiles', rotation='vertical', fontsize='xx-small')
+
+    def _plot_meta_info_ax(self, ax, meta, name_to_coord_dict, x_coords):
+        """
+        Plot up a set of meta info as categorical colors.
+        :param ax: The axis on which to plot the meta info
+        :param meta: Either 'region' or 'species'. The meta info being plotted
+        :return: None
+        """
+        width = 10
+        rectangles = []
+        for sample_uid, x_coord in name_to_coord_dict.items():
+            if self.symbiodinium_sample_uid_to_sample_name_dict[sample_uid][0] in ['S', 'P']:
+                if meta == 'region':
+                    c = self.region_color_dict[self.all_samples_df.at[self.symbiodinium_sample_uid_to_sample_name_dict[sample_uid], 'region']]
+                    rectangles.append(Rectangle(
+                        (x_coord - width / 2, 0),
+                        width,
+                        1, color=c))
+                elif meta == 'species':
+                    rectangles.append(Rectangle(
+                        (x_coord - width / 2, 0),
+                        width,
+                        1, color=self.species_color_dict[self.symbiodinium_sample_uid_to_sample_name_dict[sample_uid][0]]))
+            else:
+                # negative sample
+                rectangles.append(Rectangle(
+                    (x_coord - width / 2, 0),
+                    width,
+                    1, color='black'))
+        patches_collection = PatchCollection(rectangles, match_original=True)
+        ax.add_collection(patches_collection)
+        ax.set_xlim((x_coords[0] - width, x_coords[-1] + width))
+        # Remove the axis ticks
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_ylabel(meta, rotation='vertical', fontsize='xx-small')
+
 class BuitragoHier(Buitrago):
     """
     Plot up a series of dendrograms
@@ -295,7 +448,7 @@ class BuitragoHier(Buitrago):
         for sample_uid, x_coord in self.sample_name_to_x_coord_dict.items():
             if self.symbiodinium_sample_uid_to_sample_name_dict[sample_uid][0] in ['S', 'P']:
                 if meta == 'region':
-                    c = self.region_color_dict[self.all_samples_df.at[self.symbiodinium_sample_uid_to_sample_name_dict[sample_uid], 'REGION']]
+                    c = self.region_color_dict[self.all_samples_df.at[self.symbiodinium_sample_uid_to_sample_name_dict[sample_uid], 'region']]
                     rectangles.append(Rectangle(
                         (x_coord - width / 2, 0),
                         width,
@@ -430,7 +583,8 @@ class BuitragoBars(Buitrago):
 # BuitragoOrdinations(dist_type='uf')
 
 # For plotting the dendrogram figure with associated meta info and sequences
-# BuitragoHier(dist_type='uf')
+# BuitragoHier(dist_type='bc')
+BuitragoHier_split_species(dist_type='bc')
 
 # For plotting the north to south genera, sequence, and profile bars for each species
-BuitragoBars()
+# BuitragoBars()
